7206e7d853f7d0185ed566fc172563e6b04a11e4
FrancisHenriquez <francishenriquez07@gmail.com>
2025-07-10 23:13:46 -0400

    Fetching Data using server component

--- components/HomeProperties.jsx
+++ components/HomeProperties.jsx
@@ -1,35 +1,47 @@
-import properties from '@/properties.json'
 import PropertyCard from './PropertyCard';
 import Link from 'next/link';
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
 
+const HomeProperties = async () => {
+  await connectDB();
 
-const HomeProperties = () => {
-    const recentProperties = properties.slice(0, 3);
-    return (<> <section className='px-4 py-6'>
-      <div className='container-xl lg:container m-auto px-4 py-6'>
-        <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">Recent Properties</h2>
-        {properties.length === 0 ? (
-          <p>No properties found.</p>
-        ) : (
-          <div className='grid gird-cols-1 md:grid-cols-3 gap-6'>
-            {recentProperties.map((property) => (
-              <PropertyCard key={property._id} property={property} />
-            ))}
+  const recentProperties = await Property.find({})
+    .sort({ createdAt: -1 })
+    .limit(3)
+    .lean();
 
-          </div>
-        )}
-      </div>
-    </section>;
-
-    <section className='m-auto max-w-lg my-6 px-6'>
-        <Link href="/properties" className='block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700'> View All Properties</Link>
-
-    </section>
+  return (
+    <>
+      {' '}
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
+            Recent Properties
+          </h2>
+          {recentProperties.length === 0 ? (
+            <p>No properties found.</p>
+          ) : (
+            <div className="grid gird-cols-1 md:grid-cols-3 gap-6">
+              {recentProperties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+      ;
+      <section className="m-auto max-w-lg my-6 px-6">
+        <Link
+          href="/properties"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          {' '}
+          View All Properties
+        </Link>
+      </section>
     </>
-    
-    
+  );
+};
 
-)
-}
- 
 export default HomeProperties;
