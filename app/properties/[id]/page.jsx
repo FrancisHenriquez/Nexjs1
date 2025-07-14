c3a3635f2469d2fe4b8da4ae2473ab5948dc8f37
Francis Henriquez Matos <francishenriquez07@gmail.com>
2025-07-14 14:04:27 -0400

    property details component added

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,4 +1,5 @@
 import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import PropertyDetails from "@/components/Porperty.Details";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import Link from "next/link";
@@ -22,10 +23,10 @@ const PropertyPage = async ({ params }) => {
           </Link>
         </div>
       </section>
-      <section class="bg-blue-50">
-        <div class="container m-auto py-10 px-6">
-          <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-            {/*Property Info */}
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <PropertyDetails property={property} />
           </div>
         </div>
       </section>
