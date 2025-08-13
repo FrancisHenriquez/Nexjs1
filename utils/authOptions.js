56d2437c07a4494b0ff9b212c5e5437103d81143
FrancisHenriquez <francishenriquez07@gmail.com>
2025-08-13 19:40:01 -0400

    User import error fixed and Signout and profile picture added

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,6 +1,6 @@
 import GoogleProvider from 'next-auth/providers/google';
 import connectDB from '@/config/database';
-import { User } from '@/models/User';
+import User from '@/models/User';
 
 export const authOptions = {
   providers: [
@@ -18,34 +18,32 @@ export const authOptions = {
   ],
   callbacks: {
     // Invoked on successful sing in
-    async singIn({ profile }) {
-      await connectDB();
-
-      const userExists = await User.findOne({ email: profile.email });
-
-      if (!userExists) {
-        const username = profile.name.slice(0, 20);
-        await User.create({
-          email: profile.email,
-          username: username,
-          image: profile.picture
-        });
+    async signIn({ profile }) {
+      try {
+        await connectDB();
+        const userExists = await User.findOne({ email: profile.email });
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(/\s+/g, '').toLowerCase(),
+            image: profile.picture
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log('Error checking user: ', error);
+        return false;
       }
-      return true;
+    },
+    // Session callback function that modifies the session object
+    async session({ session }) {
+      //     Get user from database
+      const user = await User.findOne({ email: session.user.email });
+      //     Assign user id from the session
+      session.user.id = user._id.toString();
+      //     Return session
+      return session;
+      // }
     }
-    // Connect to the FaDatabase
-    // Check if user exists
-    // If not, create user
-    // Return true to allow sing in
-  },
-  // Session callback function that modifies the session object
-  async session({ session }) {
-    //     Get user from database
-    const user = await User.findOne({ email: session.user.email });
-    //     Assign user id from the session
-    session.user.id = user._id.toString();
-    //     Return session
-    return session;
-    // }
   }
 };
