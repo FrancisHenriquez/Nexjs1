0593cd1a500ef4fbde3bc19aa7f6ee42b32903e7
Francis Henriquez Matos <francishenriquez07@gmail.com>
2025-08-18 12:51:15 -0400

    property add action added

--- components/PropertyAddForm.jsx
+++ components/PropertyAddForm.jsx
@@ -1,9 +1,11 @@
-const PropertyAddHtmhtmlForm = () => {
+import addProperty from "@/app/actions/addProperty";
+
+const PropertyAddForm = () => {
   return (
     <section className="bg-blue-50">
       <div className="container m-auto max-w-2xl py-24">
         <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
-          <form>
+          <form action={addProperty}>
             <h2 className="text-3xl text-center font-semibold mb-6">
               Add Property
             </h2>
@@ -430,4 +432,4 @@ const PropertyAddHtmhtmlForm = () => {
   );
 };
 
-export default PropertyAddHtmhtmlForm;
+export default PropertyAddForm;
