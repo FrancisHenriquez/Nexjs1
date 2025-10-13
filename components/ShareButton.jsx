57603b14f23d9ba2c0062ff9b917a644f9636834
FrancisHenriquez <francishenriquez07@gmail.com>
2025-10-13 17:57:30 -0400

    Share Buttom and search function added

--- components/ShareButton.jsx
+++ components/ShareButton.jsx
@@ -1,11 +1,70 @@
-import { FaShare } from 'react-icons/fa';
+'use client';
+import {
+  FacebookShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  EmailShareButton,
+  RedditShareButton,
+  TelegramShareButton,
+  FacebookIcon,
+  WhatsappIcon,
+  TwitterIcon,
+  EmailIcon,
+  RedditIcon,
+  TelegramIcon
+} from 'react-share';
 
 const ShareButtons = ({ property }) => {
+  const shareUrl = `${process.env.NEXT_PUBLIC_DOMAIN}/properties/${property._id}`;
+  const propertyTypeSlug = property?.type
+    ? property.type.replace(/\s/g, '')
+    : 'Property';
+
   return (
-    <button className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center">
-      <FaShare className="fas fa-share mr-2" />
-      Share Property
-    </button>
+    <>
+      <h3 className="text-xl font-bold text-center">Share This Property</h3>
+      <div className="flex gap-3 justify-center pb-5">
+        <FacebookShareButton
+          url={shareUrl}
+          quote={property.name}
+          hashtag={`#${propertyTypeSlug}ForRent`}
+        >
+          <FacebookIcon size={40} round={true} />
+        </FacebookShareButton>
+
+        <TwitterShareButton
+          url={shareUrl}
+          title={property.name}
+          hashtags={[`#${propertyTypeSlug}ForRent`]}
+        >
+          <TwitterIcon size={40} round={true} />
+        </TwitterShareButton>
+
+        <WhatsappShareButton
+          url={shareUrl}
+          title={property.name}
+          separator="::"
+        >
+          <WhatsappIcon size={40} round={true} />
+        </WhatsappShareButton>
+
+        <EmailShareButton
+          url={shareUrl}
+          subject={property.name}
+          body={`Check out this property listing: ${shareUrl}`}
+        >
+          <EmailIcon size={40} round={true} />
+        </EmailShareButton>
+
+        <RedditShareButton url={shareUrl} title={property.name}>
+          <RedditIcon size={40} round={true} />
+        </RedditShareButton>
+
+        <TelegramShareButton url={shareUrl} title={property.name}>
+          <TelegramIcon size={40} round={true} />
+        </TelegramShareButton>
+      </div>
+    </>
   );
 };
 
