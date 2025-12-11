b6b5f86e3e8febfb7dc05da92c56c76148a84f57
FrancisHenriquez <francishenriquez07@gmail.com>
2025-12-11 18:49:22 -0400

    Delete buttom and action added

--- components/MessageCard.jsx
+++ components/MessageCard.jsx
@@ -3,15 +3,24 @@
 import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { markMessageAsRead } from '@/app/actions/markMessageAsRead';
+import deleteMessage from '@/app/actions/deleteMessage';
 
 const MessageCard = ({ message }) => {
   const [isRead, setIsRead] = useState(message.read);
+
+  const [isDeleted, setIsDeleted] = useState(false);
 
   const handleReadClick = async () => {
     const read = await markMessageAsRead(message._id);
 
     setIsRead(read);
-    toast.success(`Marked as ${read ? 'read' : 'new'}`);
+    toast.success(`Marked as ${read ? 'Read' : 'New'}`);
+  };
+
+  const handelDeleteClick = async () => {
+    await deleteMessage(message._id);
+    setIsDeleted(true);
+    toast.success('Message Deleted');
   };
 
   const propertyName =
@@ -24,6 +33,10 @@ const MessageCard = ({ message }) => {
   const receivedDate = new Date(message.createdAt).toLocaleDateString('en-US', {
     timeZone: 'UTC'
   });
+
+  if (isDeleted) {
+    return <p>Deleted Message</p>;
+  }
 
   return (
     <div className="relative bg-white p-3 rounded-md shadow-sm border border-gray-200">
@@ -42,7 +55,10 @@ const MessageCard = ({ message }) => {
       <ul className="mt-3 space-y-1 text-sm">
         <li className="flex gap-1">
           <strong>Reply Email:</strong>
-          <a href={`mailto:${message.email}`} className="text-blue-500 hover:underline">
+          <a
+            href={`mailto:${message.email}`}
+            className="text-blue-500 hover:underline"
+          >
             {message.email}
           </a>
         </li>
@@ -50,7 +66,10 @@ const MessageCard = ({ message }) => {
         <li className="flex gap-1">
           <strong>Reply phone:</strong>
           {''}
-          <a href={`tel:${message.phone}`} className="text-blue-500 hover:underline">
+          <a
+            href={`tel:${message.phone}`}
+            className="text-blue-500 hover:underline"
+          >
             {message.phone}
           </a>
         </li>
@@ -67,7 +86,10 @@ const MessageCard = ({ message }) => {
       >
         {isRead ? 'Mark as New' : 'Mark as Read'}
       </button>
-      <button className="mt-3 bg-red-500 text-white py-1 px-3 rounded text-sm hover:bg-red-600">
+      <button
+        onClick={handelDeleteClick}
+        className="mt-3 bg-red-500 text-white py-1 px-3 rounded text-sm hover:bg-red-600"
+      >
         Delete
       </button>
     </div>
